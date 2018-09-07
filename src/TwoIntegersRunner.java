b2bb015157fc0e823e9c977b444000de72e8d6ff
bob <bob@APCSA>
2018-09-07 15:33:40 -0700

    working

--- src/TwoIntegersRunner.java
+++ src/TwoIntegersRunner.java
@@ -3,6 +3,18 @@ public class TwoIntegersRunner
 {
 	public static void main (String [] args) {
 		
+		Scanner input = new Scanner (System.in);
+		System.out.println("enter a number: ");
+		int num1 = input.nextInt();
+		System.out.println("Enter second number: ");
+		int num2 = input.nextInt();
+		
+		TwoInteger twointeger = new TwoInteger(num1, num2);
+		String a = twointeger.arithmetic();
+		System.out.println(a);
+		
+
+			
 			
 	}
 }
